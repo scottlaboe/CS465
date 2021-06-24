a6fe3d40b7213d0444d1b9f2c1e91f666d2bd240
scottlaboe <54285870+scottlaboe@users.noreply.github.com>
2021-06-24 11:00:46 -0400

    initial commit

--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -5,10 +5,12 @@ const ctrlMain = require('../controllers/trips');
 /* GET home page */
 router
     .route('/trips')
-    .get(ctrlMain.tripsList);
+    .get(ctrlMain.tripsList)
+    .post(ctrlMain.tripsAddTrip);
 
 router
     .route('/trips/:tripCode')
-    .get(ctrlMain.tripsFindByCode);
+    .get(ctrlMain.tripsFindByCode)
+    .put(ctrlMain.tripsUpdateTrip);
 
 module.exports = router;
